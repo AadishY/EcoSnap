523da0a80cadc4c57fb26f89411c5af408b48c46
Aadish <101470733+AadishY@users.noreply.github.com>
2024-08-30 22:36:32 +0530

    Update EcoTalk.py

--- pages/EcoTalk.py
+++ pages/EcoTalk.py
@@ -7,7 +7,7 @@ import random
 load_dotenv()
 
 aadish = ["llama-3.1-8b-instant", "gemma-7b-it", "gemma2-9b-it"]
-model = random.choice(a)
+model = random.choice(aadish)
 #"llama-3.1-8b-instant", "llama-3.1-70b-versatile", "gemma-7b-it", "gemma2-9b-it"
 
 # Get the API key from environment variables
