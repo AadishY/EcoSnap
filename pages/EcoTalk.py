ede3b656d1c0b20828460762dae470504ead5926
Aadish <101470733+AadishY@users.noreply.github.com>
2024-08-30 22:24:17 +0530

    Update EcoTalk.py

--- pages/EcoTalk.py
+++ pages/EcoTalk.py
@@ -5,7 +5,7 @@ from groq import Groq
 
 # Load environment variables from .env file
 load_dotenv()
-model="gemma2-9b-it"
+model="llama-3.1-8b-instant"
 #"llama-3.1-8b-instant", "llama-3.1-70b-versatile", "gemma-7b-it", "gemma2-9b-it"
 
 # Get the API key from environment variables
