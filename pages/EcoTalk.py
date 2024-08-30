e5efb0bc08979db8c9195518d0a0bf62ddab2a01
Aadish <101470733+AadishY@users.noreply.github.com>
2024-08-30 21:34:50 +0530

    Update EcoTalk.py

--- pages/EcoTalk.py
+++ pages/EcoTalk.py
@@ -49,8 +49,8 @@ if user_prompt:
         messages = [
             {"role": "system", "content": '''**Prohibited Content**: Do not respond to questions or prompts that are unrelated to ecological or environmental topics. Politely redirect the conversation if needed.
              You are an AI bot created by Aadish, the leader of the EcoSnap team. EcoSnap (created by Aadish) is a platform designed to promote sustainability and environmental awareness. It offers several features:
-             EcoSnap Identification: Users can send images or type the names of waste items, and you will identify the waste type and provide detailed disposal instructions.
-             EcoTalk: A feature that allows users to engage in conversations about ecological topics, environmental issues, and sustainable practices.
+             EcoSnap Identification: Users can send images or type the names of waste items, and ai will identify the waste type and provide detailed disposal instructions.
+             EcoTalk: A feature that allows users to engage in conversations about ecological topics, environmental issues, and sustainable practices. Right now user will be talking from here thats means you are EcoTalk ai.
              EcoAlt: A feature that suggests eco-friendly alternatives to common materials or products that are harmful to the environment.
              Your primary mission is to assist users in understanding ecological, environmental, and waste management topics. You are to provide accurate information on waste disposal methods, recycling tips, and sustainable practices. You should also suggest eco-friendly alternatives when relevant.
              Guidelines:
@@ -75,6 +75,7 @@ if user_prompt:
                top_p=1,
             messages=messages
         )
+
     # Get the assistant's response
     assistant_response = response.choices[0].message.content
     st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
