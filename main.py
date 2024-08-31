b082f50521787abbd5aa51b03eee0b26b76143b9
Aadish <101470733+AadishY@users.noreply.github.com>
2024-08-31 17:49:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
 import streamlit as st
+from streamlit_extras.badges import badge
 
+
+# --- PAGE SETUP ---
 st.set_page_config(
-    page_title="EcoSnap"
-
+    
+    layout="wide",
+    page_title="EcoSnap",
+    page_icon=":earth_africa:",
 )
-st.title("Maine Page")
-st.sidebar.success("Select a page above.")
- 
+
+st.markdown(
+    """
+    <style>
+    .st-emotion-cache-1wbqy5l.e3g6aar2 {
+        display: none;
+    }
+    .stActionButton span[data-testid="stActionButtonLabel"] {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# Define pages
+about_page = st.Page(
+    "ch/home.py",
+    title="Home",
+    icon="🏠",
+    default=True,
+)
+project_1_page = st.Page(
+    "ch/EcoSnap.py",
+    title="EcoSnap",
+    icon="📸",
+)
+project_2_page = st.Page(
+    "ch/EcoAlt.py",
+    title="EcoAlt",
+    icon="🌱",
+)
+project_3_page = st.Page(
+    "ch/EcoTalk.py",
+    title="EcoTalk",
+    icon="🗣️",
+)
+
+# --- NAVIGATION SETUP [WITH SECTIONS] ---
+pg = st.navigation(
+    {
+        "Info": [about_page],
+        "Projects": [project_1_page, project_2_page, project_3_page],
+    }
+)
+
+# --- SHARED ON ALL PAGES ---
+st.sidebar.markdown(
+    '''
+    Created by Aadish ✨ <a href="https://github.com/AadishY">
+    <img src="https://img.shields.io/badge/Click_Here_To_See_My-Profile-blue" alt="GitHub">
+    </a>
+    ''', unsafe_allow_html=True
+)
+
+
+
+pg.run()  # Run navigation outside of the sidebar context
