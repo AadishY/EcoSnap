1ea3b65edb56c54ac430e862b4b0db90b1e14108
Aadish <101470733+AadishY@users.noreply.github.com>
2024-08-31 20:07:11 +0530

    Update EcoSnap.py

--- ch/EcoSnap.py
+++ ch/EcoSnap.py
@@ -129,13 +129,13 @@ st.markdown(
 
 # Create a container for the input fields and align them in the same line
 with st.container():
-    col1, col3, col2 = st.columns([2, 0.2, 2]) # Two columns with equal width
+    col1, col3, col2 = st.columns([2, 0.1, 2]) # Two columns with equal width
     
     # Change: Swap the input box and file uploader components
     with col1:
         uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"], key="image_upload", label_visibility="collapsed")
     with col3:
-        st.write("")
+        st.write("OR")
     with col2:
         input_text = st.text_area("Input Prompt (Garbage Name):", key="input", label_visibility="collapsed", placeholder="Enter garbage name here...")
 
